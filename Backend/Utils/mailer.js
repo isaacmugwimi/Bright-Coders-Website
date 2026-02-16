6a432affa4813ecf85ba0edf9ab04c91e8d1e859
Isaac <mugwimiisaac230@gmail.com>
2026-02-16 17:28:18 +0300

    troubleshooting email api

--- Backend/Utils/mailer.js
+++ Backend/Utils/mailer.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 import fs from "fs";
-import { EMAIL_SENDERS } from "./emailSenders";
+import { EMAIL_SENDERS } from "./emailSenders.js";
 
 // Initialize Resend with your API Key from Render Environment Variables
 const resend = new Resend(process.env.RESEND_API_KEY);
