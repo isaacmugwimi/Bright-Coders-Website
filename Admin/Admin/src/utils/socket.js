2121161af02d7f93c80b456336549218a36f2e0b
Isaac <mugwimiisaac230@gmail.com>
2026-02-27 21:02:13 +0300

    solving sockets error

--- Admin/Admin/src/utils/socket.js
+++ Admin/Admin/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL 
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
